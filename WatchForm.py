2d0216cbae33e1b3bc711de277beb91901e55bcf
122334444 <mr.savishenko@mail.ru>
2020-01-07 13:18:58 +0300

    Clear watch.

--- WatchForm.py
+++ WatchForm.py
@@ -47,7 +47,6 @@ class WatchForm(npyscreen.FormBaseNew):
         else:
             self.parentApp.getForm('WATCH').watch_thread = 1
 
-
     def update_time(self):
         while True:
             if self.watch_thread == 1:
